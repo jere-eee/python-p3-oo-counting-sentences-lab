43bbafb348dca71b699a21a65a16fd86e9e7eb63
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-27 11:28:21 +0300

    passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value='') -> None:
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, val):
+    if isinstance(val, str):
+      self._value = val
+    else:
+      print('The value must be a string.')
+    
+  def is_sentence(self):
+    if self.value[-1] == ".":
+      return True
+    else:
+      return False
+  
+  def is_question(self):
+    if self.value[-1] == "?":
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if self.value[-1] == "!":
+      return True
+    else:
+      return False
+  
+  def count_sentences(self):
+    count = 0
+    for w in self.value.split(" "):
+      if w and w[-1] == ".": 
+        count += 1
+      elif w and w[-1] == "?":
+        count += 1
+      elif w and w[-1] == "!":
+        count += 1
+    return count     
